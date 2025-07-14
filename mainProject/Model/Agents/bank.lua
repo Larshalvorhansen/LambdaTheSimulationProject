697635718aefea595368c973b855ac1adab06945
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-07-14 16:40:12 +0200

    Add: agents and agent variables

---
+++ mainProject/Model/Agents/bank.lua
@@ -0,0 +1,1 @@
+List bank variables here
---
+++ mainProject/Model/Agents/householdA.lua
@@ -0,0 +1,29 @@
+# Household scenario a
+
+income
+  x
+
+fixedCosts
+  x
+
+wage
+
+numberOfKids
+
+economicOutlook
+
+degreeOfRationality
+
+degreeOfUrbanization
+
+loan
+
+criminalBackground
+
+valueOfallAssets
+
+valueOfLiquidAssets
+
+ethnicBackground
+
+
